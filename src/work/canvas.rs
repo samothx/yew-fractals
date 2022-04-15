88443e9be8fee5dc2ffe164aee29185140c38c18
Thomas Runte <thomas@etnur.net>
2022-04-15 18:48:17 +1200

    clipboard write fails with a type error

--- src/work/canvas.rs
+++ src/work/canvas.rs
@@ -5,7 +5,7 @@ use web_sys::{ImageData, HtmlCanvasElement, CanvasRenderingContext2d, Permission
 use crate::components::root::{Config, FractalType};
 use serde::{Serialize, Deserialize};
 use super::{fractal::Points};
-use wasm_bindgen::{JsValue, JsCast, closure::Closure};
+use wasm_bindgen::{JsValue, JsCast};
 use wasm_bindgen_futures::{spawn_local, JsFuture};
 // use gloo::utils::window;
 
@@ -49,13 +49,67 @@ impl Canvas {
 
     pub fn copy_to_clipboard(&self) {
         // TODO: understand Promises & Closures in web-sys
-        info!("copy_to_clipboard: entered");
-        let query_obj = Object::from(
-            JsValue::from_serde(&QueryObject {
-                name: String::from("clipboard-write"),
-            })
-                .expect("unable to create JSValue"),
-        );
+        info!("copy_to_clipboard: entered, preparing imagedata");
+        let performance = window()
+            .expect("Window not found")
+            .performance()
+            .expect("performance should be available");
+
+        let start = performance.now();
+
+        let image_data = self.get_2d_context()
+            .get_image_data(
+                0.0,
+                0.0,
+                self.canvas.width().into(),
+                self.canvas.height().into(),
+            )
+            .expect("failed to retrieve image data")
+            .dyn_into::<ImageData>()
+            .expect("Failed to cast to ImageData").data().clone();
+
+        info!("copy_to_clipboard: converting to png at offset: {:.3} secs", (performance.now() - start) / 1000.0);
+        let mut img_u8: Vec<u8> = Vec::new();
+        let items = match write_rgba_from_u8(&mut img_u8,
+                                             &(*image_data)[..],
+                                             self.canvas.width(),
+                                             self.canvas.height()) {
+            Ok(()) => {
+                // Create an U8Array
+                info!("copy_to_clipboard: creating JsArray from png at offset: {:.3} secs", (performance.now() - start) / 1000.0);
+                let u8_array = Array::new();
+                img_u8.iter().for_each(|val| {
+                    u8_array.push(&JsValue::from(*val));
+                });
+                // Create Blob from type & U8Array
+                let mut options = BlobPropertyBag::new();
+                options.type_("image/png");
+                info!("copy_to_clipboard: creating Blob from JsArray at offset: {:.3} secs", (performance.now() - start) / 1000.0);
+                let blob = Blob::new_with_u8_array_sequence_and_options(&JsValue::from(u8_array), &options)
+                    .expect("Failed to create blob");
+                // Create ClipboardItem from Blob
+                info!("copy_to_clipboard: creating ClipboardItem from Blob  at offset: {:.3} secs", (performance.now() - start) / 1000.0);
+                let items_obj = Object::new();
+                Reflect::set(&items_obj, &blob.type_().into(), &blob)
+                    .expect("Failed to write blob to object ");
+                let item = ClipboardItem::from(JsValue::from(items_obj));
+                info!("copy_to_clipboard: got ClipboardItem: {:?}", item);
+                // Create array of ClipboardItems
+                info!("copy_to_clipboard: creating Array of ClipboardItems at offset: {:.3} secs", (performance.now() - start) / 1000.0);
+                let items = js_sys::Array::new();
+                items.push(&item);
+                items
+            },
+            Err(err) => {
+                error!("Failed to create png from imagedata, error: {:?}", err);
+                return;
+            }
+        };
+
+        info!("copy_to_clipboard: creating query for clipboard permissions at offset: {:.3} secs", (performance.now() - start) / 1000.0);
+        let query_obj = Object::new();
+        Reflect::set(&query_obj, &JsValue::from("name"), &&JsValue::from("clipboard-write"))
+            .expect("Failed to write blob to object ");
 
         // info!("copy_to_clipboard: got query_object: {:?}", query_obj);
         match window().expect("Window not found")
@@ -66,79 +120,26 @@ impl Canvas {
         {
             Ok(result) => {
                 info!("copy_to_clipboard: query permission returned ok");
+                spawn_local(async move {
+                    let query_res = JsFuture::from(result)
+                        .await
+                        .expect("Query promise was rejected");
+                    let status = PermissionStatus::from(query_res);
+                    // info!("copy_to_clipboard: got PermissionStatus {:?}", status.state());
+                    if status.state() == PermissionState::Granted {
+                        info!("copy_to_clipboard: got permission to copy to clipboard");
 
-                let image_data = self.get_2d_context()
-                    .get_image_data(
-                        0.0,
-                        0.0,
-                        self.canvas.width().into(),
-                        self.canvas.height().into(),
-                    )
-                    .expect("failed to retrieve image data")
-                    .dyn_into::<ImageData>()
-                    .expect("Failed to cast to ImageData").data().clone();
-                let mut img_u8: Vec<u8> = Vec::new();
-                match write_rgba_from_u8(&mut img_u8,
-                                         &(*image_data)[..],
-                                         self.canvas.width(),
-                                         self.canvas.height()) {
-                    Ok(()) => {
-                        // Create an U8Array
-                        let u8_array = Array::new();
-                        img_u8.iter().for_each(|val| {
-                            u8_array.push(&JsValue::from(*val));
-                        });
-                        // Create Blob from type & U8Array
-                        let mut options =  BlobPropertyBag::new();
-                        options.type_("image/png");
-                        let blob = Blob::new_with_u8_array_sequence_and_options(&JsValue::from(u8_array),&options)
-                            .expect("Failed to create blob");
-                        // Create ClipboardItem from Blob
-                        let items_obj = Object::new();
-                        Reflect::set(&items_obj, &blob.type_().into(), &blob)
-                            .expect("Failed to write blob to object ");
-                        let item = ClipboardItem::from(JsValue::from(items_obj));
-                        // Create array of ClipboardItems
-                        let items = js_sys::Array::new();
-                        items.push(&item);
-
-                        spawn_local(async move {
-                            let resolve = Closure::wrap(Box::new(move |js_value: JsValue| {
-                                // info!("copy_to_clipboard: got Result {:?}", js_value);
-
-                                let status = PermissionStatus::from(js_value);
-                                // info!("copy_to_clipboard: got PermissionStatus {:?}", status.state());
-                                if status.state() == PermissionState::Granted {
-                                    info!("copy_to_clipboard: got permission to copy to clipboard");
-                                    let clipboard = window().expect("Window not found")
-                                        .navigator()
-                                        .clipboard().expect("Clipboard not found");
-                                    JsFuture::from(clipboard.write(&items)).await;
-
-                                    // TODO: do something
-                                }
-                            }) as Box<dyn FnMut(JsValue)>);
-
-                            let reject = Closure::wrap(Box::new(|js_value: JsValue| {
-                                warn!(
-                            "copy_to_clipboard: get permission: error {}",
-                            js_value.as_string().unwrap_or("None".to_string())
-                        );
-                            }) as Box<dyn FnMut(JsValue)>);
-
-                            let promise = result.then2(&resolve, &reject);
-                            let _res = JsFuture::from(promise).await;
-                            // info!("copy_to_clipboard: spawned future returned {:?}", res);
-                        });
+                        let clipboard = window().expect("Window not found")
+                            .navigator()
+                            .clipboard().expect("Clipboard not found");
+                        match JsFuture::from(clipboard.write(&items)).await {
+                            Ok(_res) => info!("copy_to_clipboard: png image copied to clipboard"),
+                            Err(err) => error!("copy_to_clipboard: failed to copy png image to clipboard, error: {:?}", err)
+                        }
+                    } else {
+                        warn!("copy_to_clipboard: not permitted to copy to clipboard: {:?}", status.state());
                     }
-                    Err(err) => {
-                        error!("Failed to create png from imagedata, error: {:?}", err);
-                        return;
-                    }
-                }
-
-
-                // info!("copy_to_clipboard: spawned future locally");
+                });
             }
             Err(err) => {
                 warn!(
